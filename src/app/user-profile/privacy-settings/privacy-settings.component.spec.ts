0ff7566b62e976a7d971fbee9cfc1fe139968300
Christian Badura <christian.badura@capgemini.com>
2024-03-05 12:52:17 +0100

    fix: try one test for pipeline

--- src/app/user-profile/privacy-settings/privacy-settings.component.spec.ts
+++ src/app/user-profile/privacy-settings/privacy-settings.component.spec.ts
@@ -1,81 +1,82 @@
-// import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing'
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing'
 
-// import { PrivacySettingsComponent } from './privacy-settings.component'
-// import { HttpClientTestingModule } from '@angular/common/http/testing'
-// import { HttpClient } from '@angular/common/http'
-// import { TranslateModule, TranslateLoader } from '@ngx-translate/core'
+import { PrivacySettingsComponent } from './privacy-settings.component'
+import { HttpClientTestingModule } from '@angular/common/http/testing'
+import { HttpClient } from '@angular/common/http'
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core'
 // import { HttpLoaderFactory } from '../../shared/shared.module'
-// import { AUTH_SERVICE } from '@onecx/portal-integration-angular'
-// import { NO_ERRORS_SCHEMA } from '@angular/core'
-// import { By } from '@angular/platform-browser'
+import { AUTH_SERVICE, AppStateService, createTranslateLoader } from '@onecx/portal-integration-angular'
+import { NO_ERRORS_SCHEMA } from '@angular/core'
 
-// describe('PrivacySettingsComponent', () => {
-//   let component: PrivacySettingsComponent
-//   let fixture: ComponentFixture<PrivacySettingsComponent>
+describe('PrivacySettingsComponent', () => {
+  let component: PrivacySettingsComponent
+  let fixture: ComponentFixture<PrivacySettingsComponent>
 
-//   const authServiceSpy = jasmine.createSpyObj('AUTH_SERVICE', ['hasPermission'])
+  const authServiceSpy = jasmine.createSpyObj('AUTH_SERVICE', ['hasPermission'])
 
-//   beforeEach(waitForAsync(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [PrivacySettingsComponent],
-//       imports: [
-//         HttpClientTestingModule,
-//         TranslateModule.forRoot({
-//           loader: {
-//             provide: TranslateLoader,
-//             useFactory: HttpLoaderFactory,
-//             deps: [HttpClient]
-//           }
-//         })
-//       ],
-//       schemas: [NO_ERRORS_SCHEMA],
-//       providers: [{ provide: AUTH_SERVICE, useValue: authServiceSpy }]
-//     }).compileComponents()
-//     authServiceSpy.hasPermission.and.returnValue(true)
-//   }))
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [PrivacySettingsComponent],
+      imports: [
+        HttpClientTestingModule,
+        TranslateModule.forRoot({
+          isolate: true,
+          loader: {
+            provide: TranslateLoader,
+            useFactory: createTranslateLoader,
+            deps: [HttpClient, AppStateService]
+          }
+        })
+      ],
+      schemas: [NO_ERRORS_SCHEMA],
+      providers: [{ provide: AUTH_SERVICE, useValue: authServiceSpy }]
+    }).compileComponents()
+    authServiceSpy.hasPermission.and.returnValue(true)
+  }))
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(PrivacySettingsComponent)
-//     component = fixture.componentInstance
-//     fixture.detectChanges()
-//   })
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PrivacySettingsComponent)
+    component = fixture.componentInstance
+    fixture.detectChanges()
+  })
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy()
-//     expect(component.formGroup.value['hideMyProfile']).toBeNull()
-//     expect(component.formGroup.get('hideMyProfile')?.disabled).toBeFalse()
-//   })
+  it('should create', () => {
+    expect(component).toBeTruthy()
+    // expect(component.formGroup.value['hideMyProfile']).toBeNull()
+    // expect(component.formGroup.get('hideMyProfile')?.disabled).toBeFalse()
+  })
 
-//   it('should have hideMyProfile disabled if no permissions', () => {
-//     authServiceSpy.hasPermission.and.returnValue(false)
+  //   it('should have hideMyProfile disabled if no permissions', () => {
+  //     authServiceSpy.hasPermission.and.returnValue(false)
 
-//     component.ngOnInit()
+  //     component.ngOnInit()
 
-//     expect(component.formGroup.get('hideMyProfile')?.disabled).toBeTrue()
-//   })
+  //     expect(component.formGroup.get('hideMyProfile')?.disabled).toBeTrue()
+  //   })
 
-//   it('should emit true on applyChange', () => {
-//     const applyChangesSpy = spyOn(component.applyChanges, 'emit')
-//     component.applyChange()
+  //   it('should emit true on applyChange', () => {
+  //     const applyChangesSpy = spyOn(component.applyChanges, 'emit')
+  //     component.applyChange()
 
-//     expect(applyChangesSpy).toHaveBeenCalledOnceWith(true)
-//   })
+  //     expect(applyChangesSpy).toHaveBeenCalledOnceWith(true)
+  //   })
 
-//   it('should emit formGroup value on saveMenuMode', () => {
-//     let privacySettingsRefreshButtonDebugEl = fixture.debugElement.query(By.css('#up_privacy_settings_refresh'))
-//     expect(privacySettingsRefreshButtonDebugEl).toBeNull()
+  //   it('should emit formGroup value on saveMenuMode', () => {
+  //     let privacySettingsRefreshButtonDebugEl = fixture.debugElement.query(By.css('#up_privacy_settings_refresh'))
+  //     expect(privacySettingsRefreshButtonDebugEl).toBeNull()
 
-//     const privacySettingsChangeSpy = spyOn(component.privacySettingsChange, 'emit')
-//     const newFormValue = {
-//       hideMyProfile: 'true'
-//     }
-//     component.formGroup.patchValue(newFormValue)
+  //     const privacySettingsChangeSpy = spyOn(component.privacySettingsChange, 'emit')
+  //     const newFormValue = {
+  //       hideMyProfile: 'true'
+  //     }
+  //     component.formGroup.patchValue(newFormValue)
 
-//     component.savePrivacySettings()
-//     fixture.detectChanges()
+  //     component.savePrivacySettings()
+  //     fixture.detectChanges()
 
-//     expect(privacySettingsChangeSpy).toHaveBeenCalledOnceWith(newFormValue)
-//     privacySettingsRefreshButtonDebugEl = fixture.debugElement.query(By.css('#up_privacy_settings_refresh'))
-//     expect(privacySettingsRefreshButtonDebugEl).toBeTruthy()
-//   })
-// })
+  //     expect(privacySettingsChangeSpy).toHaveBeenCalledOnceWith(newFormValue)
+  //     privacySettingsRefreshButtonDebugEl = fixture.debugElement.query(By.css('#up_privacy_settings_refresh'))
+  //     expect(privacySettingsRefreshButtonDebugEl).toBeTruthy()
+  //   })
+  // })
+})
