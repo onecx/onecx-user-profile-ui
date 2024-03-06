5d71c09119ab69d6162b1cf5599a170e9b3ab228
Henry Täschner <129834483+HenryT-CG@users.noreply.github.com>
2024-03-06 12:54:31 +0100

    fix: initial setup (#8)

--- src/app/shared/label.resolver.spec.ts
+++ src/app/shared/label.resolver.spec.ts
@@ -1,58 +1,58 @@
-// import { Observable, of } from 'rxjs'
-// import { LabelResolver } from './label.resolver'
+import { Observable, of } from 'rxjs'
+import { LabelResolver } from './label.resolver'
 
-// let labelResolver: LabelResolver
+let labelResolver: LabelResolver
 
-// describe('LabelResolver', () => {
-//   const translateServiceSpy = jasmine.createSpyObj('TranslateService', ['get'])
+describe('LabelResolver', () => {
+  const translateServiceSpy = jasmine.createSpyObj('TranslateService', ['get'])
 
-//   const activatedRouteSpy = jasmine.createSpyObj('ActivatedRouteSnapshot', [], {
-//     routeConfig: {
-//       path: 'path'
-//     },
-//     data: {}
-//   })
+  const activatedRouteSpy = jasmine.createSpyObj('ActivatedRouteSnapshot', [], {
+    routeConfig: {
+      path: 'path'
+    },
+    data: {}
+  })
 
-//   const routerStateSpy = jasmine.createSpyObj('RouterStateSnapshot', [''])
+  const routerStateSpy = jasmine.createSpyObj('RouterStateSnapshot', [''])
 
-//   beforeEach(async () => {
-//     labelResolver = new LabelResolver(translateServiceSpy)
-//     translateServiceSpy.get.calls.reset()
-//     const dataSpy = Object.getOwnPropertyDescriptor(activatedRouteSpy, 'data')?.get as jasmine.Spy<() => {}>
-//     dataSpy.and.returnValue({})
-//   })
+  beforeEach(async () => {
+    labelResolver = new LabelResolver(translateServiceSpy)
+    translateServiceSpy.get.calls.reset()
+    const dataSpy = Object.getOwnPropertyDescriptor(activatedRouteSpy, 'data')?.get as jasmine.Spy<() => {}>
+    dataSpy.and.returnValue({})
+  })
 
-//   it('should translate if breadcrumb is present', (done: DoneFn) => {
-//     const dataSpy = Object.getOwnPropertyDescriptor(activatedRouteSpy, 'data')?.get as jasmine.Spy<() => {}>
-//     dataSpy.and.returnValue({
-//       breadcrumb: 'defined'
-//     })
-//     translateServiceSpy.get.and.returnValue(of('translation'))
+  it('should translate if breadcrumb is present', (done: DoneFn) => {
+    const dataSpy = Object.getOwnPropertyDescriptor(activatedRouteSpy, 'data')?.get as jasmine.Spy<() => {}>
+    dataSpy.and.returnValue({
+      breadcrumb: 'defined'
+    })
+    translateServiceSpy.get.and.returnValue(of('translation'))
 
-//     const obsResult = labelResolver.resolve(activatedRouteSpy, routerStateSpy) as Observable<string>
-//     obsResult.subscribe((result) => {
-//       expect(result).toBe('translation')
-//       expect(translateServiceSpy.get).toHaveBeenCalledOnceWith('defined')
+    const obsResult = labelResolver.resolve(activatedRouteSpy, routerStateSpy) as Observable<string>
+    obsResult.subscribe((result) => {
+      expect(result).toBe('translation')
+      expect(translateServiceSpy.get).toHaveBeenCalledOnceWith('defined')
 
-//       done()
-//     })
-//   })
+      done()
+    })
+  })
 
-//   it('should use route path if breadcrumb is not present', () => {
-//     const result = labelResolver.resolve(activatedRouteSpy, routerStateSpy)
+  it('should use route path if breadcrumb is not present', () => {
+    const result = labelResolver.resolve(activatedRouteSpy, routerStateSpy)
 
-//     expect(result).toBe('path')
-//     expect(translateServiceSpy.get).toHaveBeenCalledTimes(0)
-//   })
+    expect(result).toBe('path')
+    expect(translateServiceSpy.get).toHaveBeenCalledTimes(0)
+  })
 
-//   it('should return an empty string if neither breadcrumb nor route.routeConfig.path are present', () => {
-//     const routeConfigSpy = Object.getOwnPropertyDescriptor(activatedRouteSpy, 'routeConfig')?.get as jasmine.Spy<
-//       () => {}
-//     >
-//     routeConfigSpy.and.returnValue({})
-//     const result = labelResolver.resolve(activatedRouteSpy, routerStateSpy)
+  it('should return an empty string if neither breadcrumb nor route.routeConfig.path are present', () => {
+    const routeConfigSpy = Object.getOwnPropertyDescriptor(activatedRouteSpy, 'routeConfig')?.get as jasmine.Spy<
+      () => {}
+    >
+    routeConfigSpy.and.returnValue({})
+    const result = labelResolver.resolve(activatedRouteSpy, routerStateSpy)
 
-//     expect(result).toBe('')
-//     expect(translateServiceSpy.get).toHaveBeenCalledTimes(0)
-//   })
-// })
+    expect(result).toBe('')
+    expect(translateServiceSpy.get).toHaveBeenCalledTimes(0)
+  })
+})
--- src/app/shared/shared.module.spec.ts
+++ src/app/shared/shared.module.spec.ts
@@ -1,32 +1,32 @@
-// import { NO_ERRORS_SCHEMA } from '@angular/core'
-// import { TestBed } from '@angular/core/testing'
-// import { HttpClient } from '@angular/common/http'
-// import { HttpClientTestingModule } from '@angular/common/http/testing'
-// import { MfeInfo } from '@onecx/portal-integration-angular'
-// import { environment } from 'src/environments/environment'
+import { NO_ERRORS_SCHEMA } from '@angular/core'
+import { TestBed } from '@angular/core/testing'
+import { HttpClient } from '@angular/common/http'
+import { HttpClientTestingModule } from '@angular/common/http/testing'
+import { MfeInfo } from '@onecx/portal-integration-angular'
+import { environment } from 'src/environments/environment'
 
-// describe('SharedModule', () => {
-//   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-//   let httpClient: HttpClient
+describe('SharedModule', () => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  let httpClient: HttpClient
 
-//   beforeEach(() => {
-//     TestBed.configureTestingModule({
-//       imports: [HttpClientTestingModule],
-//       schemas: [NO_ERRORS_SCHEMA]
-//     })
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      schemas: [NO_ERRORS_SCHEMA]
+    })
 
-//     httpClient = TestBed.inject(HttpClient)
-//   })
+    httpClient = TestBed.inject(HttpClient)
+  })
 
-//   // TODO: correct this and do the right thing
-//   it('should return the correct basePath with mfeInfo', () => {
-//     const mfeInfo: MfeInfo = {
-//       mountPath: '',
-//       remoteBaseUrl: 'http://localhost:4200/',
-//       baseHref: '',
-//       shellName: ''
-//     }
-//     const result = mfeInfo.remoteBaseUrl + '' + environment.apiPrefix
-//     expect(result).toEqual('http://localhost:4200/bff')
-//   })
-// })
+  // TODO: correct this and do the right thing
+  it('should return the correct basePath with mfeInfo', () => {
+    const mfeInfo: MfeInfo = {
+      mountPath: '',
+      remoteBaseUrl: 'http://localhost:4200/',
+      baseHref: '',
+      shellName: ''
+    }
+    const result = mfeInfo.remoteBaseUrl + '' + environment.apiPrefix
+    expect(result).toEqual('http://localhost:4200/bff')
+  })
+})
