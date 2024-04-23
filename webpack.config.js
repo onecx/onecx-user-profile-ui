b0b3540b3c265c1a9dad0139bcb1417b64de612d
kim.tran <kim.tran@capgemini.com>
2024-04-23 09:39:56 +0200

    fix: lint issue

--- webpack.config.js
+++ webpack.config.js
@@ -33,7 +33,7 @@ const config = withModuleFederationPlugin({
     '@onecx/accelerator': { requiredVersion: 'auto', includeSecondaries: true },
     '@onecx/integration-interface': { requiredVersion: 'auto', includeSecondaries: true },
     '@onecx/keycloak-auth': { requiredVersion: 'auto', includeSecondaries: true },
-    '@onecx/portal-integration-angular': { requiredVersion: 'auto', includeSecondaries: true },
+    '@onecx/portal-integration-angular': { requiredVersion: 'auto', includeSecondaries: true }
   }),
   sharedMappings: ['@onecx/portal-integration-angular']
 })
