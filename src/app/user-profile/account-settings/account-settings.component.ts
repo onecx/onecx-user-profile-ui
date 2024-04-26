986c42ba5fdcc99586e5acd7c60b24bb30f3f72b
jsteenke <146953549+jsteenke@users.noreply.github.com>
2024-04-26 10:05:35 +0200

    feat: fix api connection to bff (#37)
    * feat: bff connection adjustments
    
    * feat: fix tests shared module
    
    * feat: test new shell adjustments

--- src/app/user-profile/account-settings/account-settings.component.ts
+++ src/app/user-profile/account-settings/account-settings.component.ts
@@ -1,17 +1,16 @@
 import { Component, EventEmitter, OnInit, Output, ViewChild } from '@angular/core'
 import { Router } from '@angular/router'
 
+import { ConfigurationService, PortalMessageService } from '@onecx/portal-integration-angular'
 import {
-  ConfigurationService,
+  UpdateUserSettings,
+  UserProfileAPIService,
   UserPerson,
-  UserService,
-  UserProfileAccountSettings,
-  UserProfilePreference,
-  PortalMessageService
-} from '@onecx/portal-integration-angular'
-import { UserProfileService } from '../user-profile.service'
-// import { EditPreference } from '../model/editPreference'
+  UserProfileAccountSettings
+} from 'src/app/shared/generated'
 import { PrivacySettingsComponent } from '../privacy-settings/privacy-settings.component'
+import { map } from 'rxjs/operators'
+import { Observable } from 'rxjs'
 
 @Component({
   selector: 'app-account-settings',
@@ -21,10 +20,9 @@ export class AccountSettingsComponent implements OnInit {
   @Output() public editModeUpdate = new EventEmitter<boolean>()
   @ViewChild(PrivacySettingsComponent, { static: false }) privacySettings!: PrivacySettingsComponent
 
-  public personalInfo: UserPerson = {}
+  public personalInfo$: Observable<UserPerson>
   public settings: UserProfileAccountSettings = {}
   public settingsInitial: UserProfileAccountSettings = {}
-  public preferences: UserProfilePreference[] = []
   public selectedTab = 0
 
   private cacheItem = 'httpCache'
@@ -32,57 +30,58 @@ export class AccountSettingsComponent implements OnInit {
   private profileCacheItem = 'tkit_user_profile'
 
   constructor(
-    private readonly userProfileService: UserProfileService,
     private msgService: PortalMessageService,
-    private user: UserService,
+    private userProfileService: UserProfileAPIService,
     private readonly router: Router,
     private readonly confService: ConfigurationService // private readonly stateService: StateService
   ) {
-    this.user.profile$.subscribe(
-      (profile) => {
-        this.personalInfo = profile.person || {}
-      },
-      (error) => {
-        console.error('Failed to load user profile', error)
-      }
-    )
+    this.personalInfo$ = this.userProfileService.getMyUserProfile().pipe(map((profile) => profile.person || {}))
   }
 
   public ngOnInit(): void {
-    this.user.profile$.subscribe(
-      (profile) => {
-        this.settings = profile.accountSettings || {}
+    this.userProfileService.getUserSettings().subscribe({
+      next: (profile) => {
+        this.settings = profile || {}
+        this.settingsInitial = { ...this.settings }
       },
-      (error) => {
+      error: (error) => {
         console.error('Failed to load user profile', error)
       }
-    )
-    this.settingsInitial = { ...this.settings }
-    this.loadPreferences()
+    })
   }
-
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  public localeTimezoneChange(ev: any) {
-    this.settings.localeAndTimeSettings = ev
+  public localeChange(ev: any) {
+    this.settings.locale = ev
     this.saveUserSettingsInfo()
   }
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  public layoutAndThemeChange(ev: any) {
-    this.settings.layoutAndThemeSettings = ev
+  public timezoneChange(ev: any) {
+    this.settings.timezone = ev
+    this.saveUserSettingsInfo()
+  }
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  public colorSchemeChange(ev: any) {
+    this.settings.colorScheme = ev
+    this.saveUserSettingsInfo()
+  }
+  public menuModeChange(ev: any) {
+    this.settings.menuMode = ev
     this.saveUserSettingsInfo()
   }
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   public privacySettingsChange(ev: any) {
-    this.settings.privacySettings = ev
+    console.log('EV', ev)
+    this.settings.hideMyProfile = ev.hideMyProfile
     this.saveUserSettingsInfo()
   }
 
   public saveUserSettingsInfo(): void {
-    this.userProfileService.updateUserSettings(this.settings).subscribe({
-      next: () => {
+    console.log('SAVE', this.settings)
+    this.userProfileService.updateUserSettings({ updateUserSettings: this.settings as UpdateUserSettings }).subscribe({
+      next: (res) => {
         this.msgService.success({ summaryKey: 'USER_SETTINGS.SUCCESS' })
-        if (this.settings.localeAndTimeSettings?.locale) {
-          // this.confService.changeLanguage(this.settings.localeAndTimeSettings.locale)
+        this.settings = res
+        if (this.settings.locale) {
           //TODO what exactly should we reload now? SHould we notify shell/parent?
         }
       },
@@ -104,50 +103,5 @@ export class AccountSettingsComponent implements OnInit {
 
   public reloadWindow(): void {
     window.location.reload()
-  }
-
-  // TODO: Unused?
-  // clearStorageCache() {
-  //   localStorage.removeItem(this.cacheItem)
-  //   this.msgService.info({ summaryKey: 'USER_SETTINGS.CLEAR_CACHE_INFO' })
-  // }
-
-  private loadPreferences(): void {
-    this.userProfileService.getUserPreferences().subscribe(
-      (preferences: UserProfilePreference[]) => {
-        this.preferences = preferences
-      },
-      () => {
-        this.preferences = []
-      }
-    )
-  }
-
-  editPreference(editPreference: any): void {
-    const preferenceId: string = editPreference.id
-    const value: string = editPreference.value
-    this.userProfileService.updateUserPreference(preferenceId, value).subscribe(
-      (preference: UserProfilePreference) => {
-        const preferenceIndex = this.preferences.findIndex((p) => p.id == preferenceId)
-        this.preferences[preferenceIndex] = preference
-        this.msgService.success({ summaryKey: 'USER_SETTINGS.SUCCESS' })
-      },
-      () => {
-        this.msgService.error({ summaryKey: 'USER_SETTINGS.ERROR' })
-      }
-    )
-  }
-
-  deletePreference(preferenceId: any): void {
-    this.userProfileService.deleteUserPreference(preferenceId).subscribe({
-      next: () => {
-        const preferenceIndex = this.preferences.findIndex((p) => p.id == preferenceId)
-        this.preferences.splice(preferenceIndex, 1)
-        this.msgService.success({ summaryKey: 'USER_SETTINGS.SUCCESS' })
-      },
-      error: () => {
-        this.msgService.error({ summaryKey: 'USER_SETTINGS.ERROR' })
-      }
-    })
   }
 }
