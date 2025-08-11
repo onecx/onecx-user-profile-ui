cf896ef17d85adab615c309d2d9ec864fee99ccd
Henry Täschner <129834483+HenryT-CG@users.noreply.github.com>
2025-08-11 19:59:51 +0200

    fix: use lib version 5.47.4 (#391)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,8 +7,8 @@ import { RouterModule, Routes } from '@angular/router'
 import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core'
 
 import { KeycloakAuthModule } from '@onecx/keycloak-auth'
-import { createTranslateLoader, TRANSLATION_PATH, translationPathFactory } from '@onecx/angular-utils'
-import { APP_CONFIG, AppStateService, UserService } from '@onecx/angular-integration-interface'
+import { createTranslateLoader, provideTranslationPathFromMeta } from '@onecx/angular-utils'
+import { APP_CONFIG, UserService } from '@onecx/angular-integration-interface'
 import { translateServiceInitializer, PortalCoreModule } from '@onecx/portal-integration-angular'
 
 import { environment } from 'src/environments/environment'
@@ -46,12 +46,7 @@ const routes: Routes = [
       multi: true,
       deps: [UserService, TranslateService]
     },
-    {
-      provide: TRANSLATION_PATH,
-      useFactory: (appStateService: AppStateService) => translationPathFactory('assets/i18n/')(appStateService),
-      multi: true,
-      deps: [AppStateService]
-    },
+    provideTranslationPathFromMeta(import.meta.url, 'assets/i18n/'),
     provideHttpClient(withInterceptorsFromDi())
   ]
 })
